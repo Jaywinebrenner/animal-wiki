37e0020d9eb0e76d28a63774a70f0f09c8fd69f6
Ike Esquivel-Pilloud and Jay Winebrenner <ike+jay@>
2019-10-24 14:46:18 -0700

    finish ui of animal insurance quote

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,24 @@ $(document).ready(function(){
 
     $("#" + option).toggle();
 
-
   });
 });
 
+$(document).ready(function(){
+  $("#animalinsurance").submit(function(event){
+    event.preventDefault();
+    var age = parseInt($("#age").val());
+    var gender = $("select#gender").val();
+    if (age) {
+       var quote = 1000 - age;
+       if (gender === 'male' && age < 56) {
+         quote += 250;
+       }
+       $("#rate").empty().append(quote);
+       $("#quote").show();
+     } else {
+       alert('Please enter your age.');
+     }
+})
 
-//
-// $("#ratoption").click(function(){
-//   // $("#ratstats").toggle();
+  });
