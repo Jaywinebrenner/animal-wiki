28cdb380e075e1370beb40841809a07fe1174ca7
Ike Esquivel-Pilloud and Jay Winebrenner <ike@>
2019-10-24 11:43:24 -0700

    add js content, apply list, triggers, whatnot

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $("#animaloption").submit(function(event){
+    event.preventDefault();
+    var option = $("#animallist").val();
+    console.log(option);
+
+    $("#" + option).toggle();
+
+  })
+})
+
+
+//
+// $("#ratoption").click(function(){
+//   // $("#ratstats").toggle();
